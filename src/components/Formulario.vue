b8c32c1488a5685b09ae3f41f6abf8ad318765c0
Oscar Sabogal <oscarsabogal285@gmail.com>
2024-02-06 07:13:09 -0500

    feat: first commit with template formulario

---
+++ src/components/Formulario.vue
@@ -0,0 +1,64 @@
+<script setup>
+  import { ref } from 'vue';
+
+  const nombre = ref('a');
+
+  const leerNombre = (e) => {
+    nombre.value = e.target.value;
+  }
+
+</script>
+
+<template>
+  <div class="md:w-1/2">
+    <h2 class="font-black text-3xl text-center"> Seguimientos pacientes </h2>
+    <p class="text-lg mt-5 text-center mb-10">
+      Añade pacientes y <span class="text-indigo-600 font-bold">Administralos</span>
+    </p>
+
+    <form action="" class="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      {{nombre}}
+      <div class="mb-5">
+        <label for="mascota" class="block text-gray-700 uppercase font-bold">
+          Nombre mascota
+        </label>
+        <input id="mascota" type="text" placeholder="Nombre de la mascota"
+               class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+               :value="nombre"
+                @input="leerNombre"/>
+      </div>
+      <div class="mb-5">
+        <label for="propietario" class="block text-gray-700 uppercase font-bold">
+          Nombre Propietario
+        </label>
+        <input id="propietario" type="text" placeholder="Nombre del propietario"
+               class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+      </div>
+      <div class="mb-5">
+        <label for="email" class="block text-gray-700 uppercase font-bold">
+          Email
+        </label>
+        <input id="email" type="email" placeholder="Email del propietario"
+               class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+      </div>
+      <div class="mb-5">
+        <label for="alta" class="block text-gray-700 uppercase font-bold">
+          Fecha de Alta
+        </label>
+        <input id="alta" type="date"
+               class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+      </div>
+      <div class="mb-5">
+        <label for="sintomas" class="block text-gray-700 uppercase font-bold">
+          Síntomas
+        </label>
+        <textarea id="sintomas"
+                  placeholder="Describe los síntomas"
+               class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40"/>
+      </div>
+      <input type="submit"
+      class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+      value="Registrar paciente">
+    </form>
+  </div>
+</template>
